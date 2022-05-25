cadd9697420752ccfb219cf51662e5edd48ca9e5
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2022-05-25 16:22:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,7 +16,7 @@ async def start(bot, message):
     else:
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('Go Inline', switch_inline_query=''),
+            InlineKeyboardButton('Join Our Channel', url='https://t.me/Rkrishnaa'),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(START_MSG, reply_markup=reply_markup)
